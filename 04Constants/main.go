899f90c5922257b7dee0f5e8ddea0a399c5ed629
prajwalbharadwajbm <prajwalbm40@gmail.com>
2023-06-14 18:10:55 +0530

    added infoComments to 04Constants

---
+++ 04Constants/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+const str string = "Constant"
+
+func main() {
+	fmt.Println(reflect.TypeOf(str))
+	const n = 500000000
+	const d = 3e20 / n
+
+	fmt.Println(d) // prints a float value in exp form, to get it in int form, you can convert it to int64(not int32, don't even ask why!!).
+
+	fmt.Println("Before explicit conversion")
+	fmt.Println(reflect.TypeOf(d)) // Has type which is kinda contrasting to what https://gobyexample.com/constants says!!!
+
+	a := int64(d)
+
+	fmt.Println("After explicit conversion")
+	fmt.Println(reflect.TypeOf(a)) // it gets converted to int64
+
+}
